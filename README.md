c62b337041a1dec8722189a972aff4a17a56e9f1
yangkun19921001 <yang1001yk@gmail.com>
2019-09-11 11:46:06 +0800

    增加根据 userId 获取 用户信息的 API

--- README.md
+++ README.md
@@ -21,6 +21,7 @@
 | 1.0.1.1 | 1. ContactsEngine 增加按时间轮训请求在线用户;2. 优化对讲组人数在上万的情况下获取数据慢的性能问题； | 刘扬，阳坤 |
 | 1.0.1.2 | 1. 增加视频多路呼叫接口。详细信息请看 CallEngine 接口；2. 修改 PttEngine 获取所有对讲组的接口，舍弃同步获取我当前所有组的接口； | 刘扬，阳坤 |
 | 1.0.1.3 | 1. 增加了是否需要多路呼叫，默认需要。具体 API 请看 CallEngine；2. 修改电话记录重复问题；3. C++ 完善硬编码，应用只需传入 NV21 格式的数据即可。 | 刘扬，阳坤 |
+| 1.0.1.4 | 1. ContactsEngine 增加 getUser() 获取用户信息 API;           |            |
 
 标准版本嘀嗒 APK 扫码下载:
 
@@ -584,7 +585,11 @@
   void getOnlineUserCallBack(int repeatTime, IOnLineUserListener iPttDataListener)
   ```
 
-  
+- 根据 ID 获取用户信息
+
+  ```java
+  UserEntity getUser(String userId) 
+  ```
 
 ## LocationEngine
 
--- lytest/src/main/java/com/lingyi/autiovideo/test/activity/LoginActivity.java
+++ lytest/src/main/java/com/lingyi/autiovideo/test/activity/LoginActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.text.format.Formatter;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,7 +15,12 @@ import com.blankj.utilcode.util.ToastUtils;
 import com.bnc.activity.T01Helper;
 import com.bnc.activity.callback.IIsLoginListener;
 import com.bnc.activity.callback.LoginCallBack;
+import com.bnc.activity.utils.LogHelper;
 import com.lingyi.autiovideo.test.R;
+import com.lzy.okgo.OkGo;
+import com.lzy.okgo.model.Progress;
+
+import java.io.File;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -67,6 +73,9 @@ public class LoginActivity extends AppCompatActivity {
         String trim = etUser.getText().toString().trim();
         pro.setVisibility(View.VISIBLE);
         initListener();
+
+        OkGo.getInstance().init(getApplication());
+        appRequest();
     }
 
 
@@ -118,6 +127,45 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onLogin() {
                 pro.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+
+
+    public void appRequest() {
+
+
+        OkGo.<File>get("http://47.105.211.246:8080/rstoneCmd/upload/ver/dc_v2.6.3_release.apk").execute(new com.lzy.okgo.callback.FileCallback() {
+            @Override
+            public void onSuccess(com.lzy.okgo.model.Response<File> response) {
+                // 安装软件
+                LogHelper.i("APP-onSuccess-", "onSuccess");
+
+            }
+
+            @Override
+            public void onStart(com.lzy.okgo.request.base.Request<File, ? extends com.lzy.okgo.request.base.Request> request) {
+                super.onStart(request);
+                LogHelper.i("APP-onStart-", "start");
+            }
+
+            @Override
+            public void onError(com.lzy.okgo.model.Response<File> response) {
+                super.onError(response);
+                LogHelper.i("APP-onError-", "异常" + response.getException().getMessage());
+
+            }
+
+            @Override
+            public void downloadProgress(Progress progress) {
+                super.downloadProgress(progress);
+                String downloadLength = Formatter.formatFileSize(getApplicationContext(), progress.currentSize);
+                String totalLength = Formatter.formatFileSize(getApplicationContext(), progress.totalSize);
+//              tvDownloadSize.setText(downloadLength + "/" + totalLength);
+                String netSpeed = Formatter.formatFileSize(getApplicationContext(), progress.speed);
+                String pr = (Math.round(progress.currentSize * 10000) * 1.0f / 100) + "%";
+                int pro = (int) (Math.round(progress.currentSize * 10000) * 1.0f / progress.totalSize);
+                LogHelper.i("APP-downloadProgress-", pro + "");
             }
         });
     }
