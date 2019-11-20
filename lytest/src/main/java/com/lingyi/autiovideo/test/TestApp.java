9263b33fdc8a7aa947519a2a8ffd541fb978187b
DevYK <yangkun1001yk@gmail.com>
2019-11-20 17:37:40 +0800

    上传 sdkdemo apk

--- lytest/src/main/java/com/lingyi/autiovideo/test/TestApp.java
+++ lytest/src/main/java/com/lingyi/autiovideo/test/TestApp.java
@@ -2,9 +2,11 @@ package com.lingyi.autiovideo.test;
 
 import android.support.multidex.MultiDex;
 
+import com.blankj.utilcode.util.CrashUtils;
 import com.blankj.utilcode.util.Utils;
 import com.bnc.activity.PttApplication;
 import com.bnc.activity.T01Helper;
+import com.bnc.activity.service.module.log.CrashHandler;
 
 /**
  * Created by yangk on 2018/1/4.
@@ -22,13 +24,15 @@ public class TestApp extends PttApplication {
 
         Utils.init(this);
 
+        CrashHandler.getInstance().register(this);
+
     }
 
     private void initT01SDK() {
         T01Helper.getInstance().initAppContext(getApplicationContext());
         //手机不需要多路
-        T01Helper.getInstance().getCallEngine().setMultipleLines(false);
+        T01Helper.getInstance().getCallEngine().setMultipleLines(true);
         //交给Java硬编码
-        T01Helper.getInstance().getSetEngine().isJavaMediacodec(true);
+        T01Helper.getInstance().getSetEngine().setJavaMediacodec(true);
     }
 }
